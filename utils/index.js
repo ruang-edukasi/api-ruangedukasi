c17768636f795e638ad56bd4c61ca621b373a1ae
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-01 01:23:41 +0700

    update: imageKit for handle media library

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,10 @@
 const bcrypt = require("bcrypt");
+const ImageKit = require("imagekit");
 
-const encryptPassword = async(password) => {
-    const salt = await bcrypt.genSalt(5);
+const encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(5);
 
-    return bcrypt.hash(password, salt);
+  return bcrypt.hash(password, salt);
 };
 
 const encryptEmail = async () => {
@@ -19,13 +20,14 @@ const encryptEmail = async () => {
   return randomText;
 };
 
+const imageKit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
+
 module.exports = {
   encryptPassword,
   encryptEmail,
-    imageKit: new ImageKit({
-        publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-        privateKey: process.env.IMAGEKIT_SECRET_KEY,
-        urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-
-    })
+  imageKit,
 };
