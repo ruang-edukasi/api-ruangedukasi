c4a01c2256230dcea13bd4ac8b954ac7b69a0d3d
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-01 20:15:32 +0700

    update: admin verification email

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -9,20 +9,73 @@ const secretKey = process.env.JWT_KEY || "no_secret";
 const register = async (req, res) => {
   const { full_name, email, phone_number, password } = req.body;
   try {
+    const adminCheck = await admin.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (adminCheck) {
+      return res.status(203).json({
+        error: true,
+        message: "Email already registered",
+      });
+    }
+
+    const encryptEmail = await utils.encryptEmail(email);
     const data = await admin.create({
       data: {
         fullName: full_name,
         email: email,
         phoneNumber: phone_number,
         password: await utils.encryptPassword(password),
+        emailToken: encryptEmail,
+        status: "inActive",
+      },
+      select: {
+        fullName: true,
+        email: true,
+        phoneNumber: true,
+        status: true,
       },
     });
 
-    delete data["password"]; // hide password field in response
-    return res.status(201).json({
-      error: false,
-      message: "Registration successful",
-      response: data,
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    let mailOptions = {
+      from: "Ruang Edukasi <system@gmail.com>",
+      to: email,
+      subject: "Ruang Edukasi - Verification Email",
+      html: `<p>
+        To complete registration at <strong>Ruang Edukasi</strong> <br/><br/>
+        <a href="${process.env.HOST}/api/v1/auth/admin/verification-email/${encryptEmail}"
+        style="background-color: #4CAF50; color: white; padding: 10px; text-decoration:none; text-align: center;">Verification email</a>
+        </p>`,
+    };
+
+    transporter.sendMail(mailOptions, (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          error: true,
+          message: err,
+        });
+      }
+
+      delete data["password"]; // hide password field in response
+      return res.status(201).json({
+        error: false,
+        message:
+          "Registration successful.\nCheck your inbox or spam folder to verification your email.",
+      });
     });
   } catch (error) {
     return res.status(500).json({
@@ -46,6 +99,20 @@ const login = async (req, res) => {
       return res.status(404).json({
         error: true,
         message: "Email is not registered in our system",
+      });
+    }
+
+    const activeCheck = await admin.findUnique({
+      where: {
+        email: email,
+        status: "Active",
+      },
+    });
+
+    if (!activeCheck) {
+      return res.status(401).json({
+        error: true,
+        message: "Please activate your email first.",
       });
     }
 
@@ -78,7 +145,45 @@ const login = async (req, res) => {
   }
 };
 
+const verificationEmail = async (req, res) => {
+  try {
+    const emailTokenCheck = await admin.findFirst({
+      where: {
+        emailToken: req.params.key,
+      },
+    });
+
+    if (!emailTokenCheck) {
+      return res.status(500).json({
+        error: true,
+        message: "Email verification link not valid",
+      });
+    }
+
+    await admin.update({
+      data: {
+        emailToken: null,
+        status: "Active",
+      },
+      where: {
+        id: emailTokenCheck.id,
+      },
+    });
+
+    return res.status(200).json({
+      error: false,
+      message: "Your email successful verified.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: true,
+      message: error,
+    });
+  }
+};
+
 module.exports = {
   register,
   login,
+  verificationEmail,
 };
