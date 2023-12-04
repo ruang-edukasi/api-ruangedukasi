5beb9a147d673820a186456db2000a235f9fd90e
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-04 21:44:35 +0700

    update: utils for dummy account number

--- utils/index.js
+++ utils/index.js
@@ -26,8 +26,28 @@ const imageKit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
+const encryptOrder = async () => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let orderTrx = "";
+
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    orderTrx += characters.charAt(randomIndex);
+  }
+
+  return orderTrx;
+};
+
+const dummyAccountNumber = async () => {
+  const generate = Math.floor(100000000000 + Math.random() * 900000000000);
+
+  return generate.toString();
+};
+
 module.exports = {
   encryptPassword,
   encryptEmail,
+  encryptOrder,
+  dummyAccountNumber,
   imageKit,
 };
