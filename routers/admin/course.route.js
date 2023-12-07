c15954063357acf29ae4e4a57fb2531c0edb135c
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-08 01:09:17 +0700

    update: routes course method

--- routers/admin/course.route.js
+++ routers/admin/course.route.js
@@ -1,28 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/course.controller");
-const multer = require("../../middlewares/multer");
 const multerLib = require("multer")();
 const checkToken = require("../../middlewares/checkToken");
 
 router.post(
-    "/course/category",
-    multerLib.single("image_url"),
-    controller.addCategory
+  "/course/category",
+  multerLib.single("image"),
+  controller.addCategory
 );
-router.put("/course/update-category/:categoryId",
-    multerLib.single("image_url"),
-    controller.updateCategory
+router.put(
+  "/course/update-category/:categoryId",
+  multerLib.single("image"),
+  controller.updateCategory
+);
+router.post(
+  "/course/update-category/:categoryId",
+  multerLib.single("image"),
+  controller.updateCategory
 );
 router.post("/course/level", controller.addLevel);
 router.put("/course/update-level/:levelId", controller.updateLevel);
+router.post("/course/update-level/:levelId", controller.updateLevel);
 router.post("/course/type", controller.addType);
 router.put("/course/update-type/:typeId", controller.updateType);
+router.post("/course/update-type/:typeId", controller.updateType);
 router.post("/course", checkToken, controller.addCourse);
 router.post(
-    "/course/content/:courseId",
-    checkToken,
-    controller.addCourseContent
+  "/course/content/:courseId",
+  checkToken,
+  controller.addCourseContent
 );
 router.post("/course/skill/:courseId", checkToken, controller.addCourseSkill);
 router.post("/course/target/:courseId", checkToken, controller.addCourseTarget);
