58acb892dc76280c2dbc8facae56a6c75d509062
WebsiteThufail <abahc4mp3x@gmail.com>
2023-12-11 20:17:00 +0700

    update add: filter premium & gratis

--- controllers/course/type.controller.js
+++ controllers/course/type.controller.js
@@ -1,30 +1,86 @@
 const { courseType } = require("../../models");
 
-const allType = async (req, res) => {
-  try {
-    const data = await courseType.findMany({
-      orderBy: {
-        id: "asc",
-      },
-      select: {
-        id: true,
-        typeName: true,
-      },
-    });
+const allType = async(req, res) => {
+    try {
+        const data = await courseType.findMany({
+            orderBy: {
+                id: "asc",
+            },
+            select: {
+                id: true,
+                typeName: true,
+            },
+        });
 
-    return res.status(200).json({
-      error: false,
-      message: "Load course type successful",
-      response: data,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: true,
-      message: error,
-    });
-  }
+        return res.status(200).json({
+            error: false,
+            message: "Load course type successful",
+            response: data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: error,
+        });
+    }
+}
+const premiumType = async(req, res) => {
+    try {
+        const data = await courseType.findMany({
+            where: {
+                typeName: "Premium",
+            },
+            orderBy: {
+                id: "asc",
+            },
+            select: {
+                id: true,
+                typeName: true,
+            },
+        });
+
+        return res.status(200).json({
+            error: false,
+            message: "Load premium course types successful",
+            response: data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: error,
+        });
+    }
+};
+const gratisType = async(req, res) => {
+    try {
+        const data = await courseType.findMany({
+            where: {
+                typeName: "Gratis",
+            },
+            orderBy: {
+                id: "asc",
+            },
+            select: {
+                id: true,
+                typeName: true,
+            },
+        });
+
+        return res.status(200).json({
+            error: false,
+            message: "Load Gratis course types successful",
+            response: data,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: error,
+        });
+    }
 };
 
 module.exports = {
-  allType,
+    allType,
+    premiumType,
+    gratisType
 };
--- routers/course/type.route.js
+++ routers/course/type.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/course/type.controller");
 
-router.get("/", controller.allType);
+router.get("/", controller.allType)
+router.get("/premium", controller.premiumType)
+router.get("/gratis", controller.gratisType);
 
 module.exports = router;
