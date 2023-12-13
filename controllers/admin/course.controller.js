a7ccd00f0d75db0885467daca3dafb1d41182518
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-13 10:58:57 +0700

    update: all course owned by admin

--- controllers/admin/course.controller.js
+++ controllers/admin/course.controller.js
@@ -461,4 +461,101 @@ module.exports = {
       });
     }
   },
+
+  allCourseOwned: async (req, res) => {
+    try {
+      const jwtAdminId = res.sessionLogin.id; // From checktoken middlewares
+
+      const cekAkses = await admin.findUnique({
+        where: {
+          id: jwtAdminId,
+        },
+      });
+
+      if (!cekAkses) {
+        return res.status(403).json({
+          error: true,
+          message: "You don't have access in this page",
+        });
+      }
+
+      const data = await course.findMany({
+        where: {
+          adminId: jwtAdminId,
+        },
+        orderBy: {
+          id: "asc",
+        },
+        select: {
+          id: true,
+          instructorName: true,
+          courseName: true,
+          courseDescription: true,
+          imageUrl: true,
+          price: true,
+          rating: true,
+          CourseCategory: {
+            select: {
+              categoryName: true,
+            },
+          },
+          CourseType: {
+            select: {
+              typeName: true,
+            },
+          },
+          CourseLevel: {
+            select: {
+              levelName: true,
+            },
+          },
+          userCourseContent: {
+            select: {
+              id: true,
+              userId: true,
+              courseId: true,
+              courseName: true,
+            },
+          },
+        },
+      });
+
+      // Convert BigInt to string before sending the response
+      const responseData = data.map((course) => ({
+        ...course,
+        userCount: course.userCourseContent.length,
+        thumbnailCourse: course.imageUrl,
+        price: course.price ? parseFloat(course.price) : null,
+        courseType: course.CourseType.typeName,
+        courseCategory: course.CourseCategory.categoryName,
+        courseLevel: course.CourseLevel.levelName,
+      }));
+
+      const myCourseCount = responseData.length;
+
+      responseData.forEach((course) => {
+        delete course.userCourseContent;
+        delete course.CourseType;
+        delete course.CourseCategory;
+        delete course.CourseLevel;
+        delete course.imageUrl;
+      });
+
+      return res.status(200).json({
+        error: false,
+        message: "Load course successful",
+        response: {
+          adminId: jwtAdminId,
+          totalCourse: myCourseCount,
+          myCourse: responseData,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        error: true,
+        message: error || "Internal server error",
+      });
+    }
+  },
 };
