6e2fde5d58f03d66f5281fb4f934caabdb9c98e7
whywidodo <mr.wahyuwidodo@gmail.com>
2023-12-15 22:06:40 +0700

    add: filter for multiple course category

--- routers/course/search.route.js
+++ routers/course/search.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../../controllers/course/search.controller");
 
 router.get("/", controller.searchCourse);
+router.get("/multi", controller.searchMultiCourse);
 
 module.exports = router;
