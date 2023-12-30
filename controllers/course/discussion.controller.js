518526df5a32bae06e8fc560adc03c6bc89d10cc
WebsiteThufail <abahc4mp3x@gmail.com>
2023-12-31 01:21:15 +0700

    update: translate response message

--- controllers/course/discussion.controller.js
+++ controllers/course/discussion.controller.js
@@ -1,88 +1,88 @@
 const { Discussion } = require("../../models");
 
 module.exports = {
-    getCourseDiscussions: async (req, res) => {  
+    getCourseDiscussions: async(req, res) => {
         try {
             const courseId = req.params.courseId;
 
             const discussions = await Discussion.findMany({
                 where: {
-                courseId: courseId,
+                    courseId: courseId,
                 },
                 include: {
-                User: {
-                    select: {
-                    id: true,
-                    fullName: true,
-                    imageUrl: true,
-                    },
-                },
-                replies: {
-                    include: {
                     User: {
                         select: {
-                        id: true,
-                        fullName: true,
-                        imageUrl: true,
+                            id: true,
+                            fullName: true,
+                            imageUrl: true,
                         },
                     },
+                    replies: {
+                        include: {
+                            User: {
+                                select: {
+                                    id: true,
+                                    fullName: true,
+                                    imageUrl: true,
+                                },
+                            },
+                        },
                     },
-                },
                 },
             });
 
             res.json({
                 discussions,
             });
-            } catch (error) {
-            console.error("Error getting course discussions:", error);
+        } catch (error) {
+            console.error("Terjadi error saat melakukan diskusi kursus:", error);
             res.status(500).json({ message: "Internal Server Error" });
-            }
-        },
+        }
+    },
 
-    addDiscussionComment: async (req, res) => {  
+    addDiscussionComment: async(req, res) => {
         try {
-            const userId = req.user.id; 
+            const userId = req.user.id;
             const courseId = req.params.courseId;
             const { comment } = req.body;
 
             const newComment = await Discussion.create({
                 data: {
-                courseId: courseId,
-                userId: userId,
-                comment: comment,
+                    courseId: courseId,
+                    userId: userId,
+                    comment: comment,
                 },
             });
 
             res.json({
                 newComment,
             });
-            } catch (error) {
-            console.error("Error adding discussion comment:", error);
+        } catch (error) {
+            console.error("Kesalahan saat menambahkan komentar diskusi:", error);
             res.status(500).json({ message: "Internal Server Error" });
-            }
-        },
+        }
+    },
 
-    addReplyToComment: async (req, res) => { 
+    addReplyToComment: async(req, res) => {
         try {
-            const userId = req.user.id; 
+            const userId = req.user.id;
             const commentId = req.params.commentId;
             const { reply } = req.body;
 
             const newReply = await Discussion.create({
                 data: {
-                parentId: commentId,
-                userId: userId,
-                comment: reply,
+                    parentId: commentId,
+                    userId: userId,
+                    comment: reply,
                 },
-        });
+            });
 
-        res.json({
-            newReply,
-        });
+            res.json({
+                newReply,
+            });
         } catch (error) {
-        console.error("Error adding reply:", error);
-        res.status(500).json({ message: "Internal Server Error" });
+            console.error("Kesalahan saat menambahkan balasan:", error);
+            res.status(500).json({ message: "Internal Server Error" });
         }
     },
 };
--- controllers/user/oauth.controller.js
+++ controllers/user/oauth.controller.js
@@ -7,62 +7,60 @@ const utils = require("../../utils");
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.JWT_KEY || "no_secret";
 
-const googleOauthCallback = async (req, res) => {
-  const { code } = req.query;
-  const { tokens } = await oauth2Client.getToken(code.toString());
-  oauth2Client.setCredentials(tokens);
+const googleOauthCallback = async(req, res) => {
+    const { code } = req.query;
+    const { tokens } = await oauth2Client.getToken(code.toString());
+    oauth2Client.setCredentials(tokens);
 
-  const oauth2 = google.oauth2({
-    auth: oauth2Client,
-    version: "v2",
-  });
-
-  const { data } = await oauth2.userinfo.get();
-
-  // If data email and name empty
-  if (!data.email || !data.name) {
-    return res.status(200).json({
-      response: data,
+    const oauth2 = google.oauth2({
+        auth: oauth2Client,
+        version: "v2",
     });
-  }
 
-  let userExist = await user.findUnique({
-    where: {
-      email: data.email,
-    },
-  });
+    const { data } = await oauth2.userinfo.get();
 
-  if (!userExist) {
-    // If user not found
-    const encryptEmail = await utils.encryptEmail();
-    userExist = await user.create({
-      data: {
-        fullName: data.name,
-        email: data.email,
-        password: await utils.encryptPassword(encryptEmail),
-        status: "Active",
-      },
-    });
-  }
-
-  // Generate JWT token
-  const token = jwt.sign(
-    { id: userExist.id, email: userExist.email },
-    secretKey,
-    {
-      expiresIn: "6h",
+    // If data email and name empty
+    if (!data.email || !data.name) {
+        return res.status(200).json({
+            response: data,
+        });
     }
-  );
 
-  return res.status(200).json({
-    error: false,
-    message: "Login successful",
-    response: {
-      token: token,
-    },
-  });
+    let userExist = await user.findUnique({
+        where: {
+            email: data.email,
+        },
+    });
+
+    if (!userExist) {
+        // If user not found
+        const encryptEmail = await utils.encryptEmail();
+        userExist = await user.create({
+            data: {
+                fullName: data.name,
+                email: data.email,
+                password: await utils.encryptPassword(encryptEmail),
+                status: "Active",
+            },
+        });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: userExist.id, email: userExist.email },
+        secretKey, {
+            expiresIn: "6h",
+        }
+    );
+
+    return res.status(200).json({
+        error: false,
+        message: "Login berhasil",
+        response: {
+            token: token,
+        },
+    });
 };
 
 module.exports = {
-  googleOauthCallback,
+    googleOauthCallback,
 };
