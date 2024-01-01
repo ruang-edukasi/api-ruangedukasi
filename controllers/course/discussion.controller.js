cf5aa5ded2c02159560bbc525c58ad2821813169
Taufik Faturokhman <132332894+TaufikFaturokhman@users.noreply.github.com>
2024-01-01 14:59:07 +0700

    Fix ruang diskusi

--- controllers/course/discussion.controller.js
+++ controllers/course/discussion.controller.js
@@ -1,88 +1,88 @@
-const { Discussion } = require("../../models");
+const { Discussion, DiscussionReply } = require("../../models");
 
 module.exports = {
-    getCourseDiscussions: async (req, res) => {  
-        try {
-            const courseId = req.params.courseId;
+  getCourseDiscussions: async (req, res) => {
+    try {
+      const courseId = req.params.courseId;
 
-            const discussions = await Discussion.findMany({
-                where: {
-                courseId: courseId,
-                },
-                include: {
-                User: {
-                    select: {
+      const discussions = await Discussion.findMany({
+        where: {
+          courseId: courseId,
+        },
+        include: {
+          User: {
+            select: {
+              id: true,
+              fullName: true,
+              imageUrl: true,
+            },
+          },
+          replies: {
+            include: {
+              User: {
+                select: {
                     id: true,
                     fullName: true,
                     imageUrl: true,
-                    },
                 },
-                replies: {
-                    include: {
-                    User: {
-                        select: {
-                        id: true,
-                        fullName: true,
-                        imageUrl: true,
-                        },
-                    },
-                    },
-                },
-                },
-            });
-
-            res.json({
-                discussions,
-            });
-            } catch (error) {
-            console.error("Error getting course discussions:", error);
-            res.status(500).json({ message: "Internal Server Error" });
-            }
+              },
+            },
+          },
         },
+      });
 
-    addDiscussionComment: async (req, res) => {  
-        try {
-            const userId = req.user.id; 
-            const courseId = req.params.courseId;
-            const { comment } = req.body;
+      res.json({
+        discussions,
+      });
+    } catch (error) {
+      console.error("Error getting course discussions:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
 
-            const newComment = await Discussion.create({
-                data: {
-                courseId: courseId,
-                userId: userId,
-                comment: comment,
-                },
-            });
+  addDiscussionComment: async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const courseId = req.params.courseId;
+      const { comment } = req.body;
 
-            res.json({
-                newComment,
-            });
-            } catch (error) {
-            console.error("Error adding discussion comment:", error);
-            res.status(500).json({ message: "Internal Server Error" });
-            }
+      const newComment = await Discussion.create({
+        data: {
+          courseId: courseId,
+          userId: userId,
+          comment: comment,
         },
+      });
 
-    addReplyToComment: async (req, res) => { 
-        try {
-            const userId = req.user.id; 
-            const commentId = req.params.commentId;
-            const { reply } = req.body;
+      res.json({
+        newComment,
+      });
+    } catch (error) {
+      console.error("Error adding discussion comment:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
 
-            const newReply = await Discussion.create({
-                data: {
-                parentId: commentId,
-                userId: userId,
-                comment: reply,
-                },
-        });
+  addReplyToComment: async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const commentId = req.params.commentId;
+      const { reply } = req.body;
 
-        res.json({
-            newReply,
-        });
-        } catch (error) {
-        console.error("Error adding reply:", error);
-        res.status(500).json({ message: "Internal Server Error" });
-        }
-    },
+      const newReply = await DiscussionReply.create({
+        data: {
+          parentId: commentId,
+          userId: userId,
+          reply: reply,
+        },
+      });
+
+      res.json({
+        newReply,
+      });
+    } catch (error) {
+      console.error("Error adding reply:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
 };
